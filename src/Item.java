649f206cfd274036cb471d37fde4f79db3a9dc1b
YOGA <yogaibnudinataa@gmail.com>
2018-11-30 15:37:11 +0700

    Membuat getter dan setter pada class Item

--- src/Item.java
+++ src/Item.java
@@ -25,4 +25,35 @@ public class Item {
         this.jumlah = jumlah;
     }
     
+    //untuk mengambil nama
+    public String getNama() {
+        return nama;
+    }
+    
+    //untuk setting nama
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+    
+    //untuk mengambil harga
+    public float getHarga() {
+        return harga;
+    }
+    
+    //untuk setting harga
+    public void setHarga(float harga) {
+        this.harga = harga;
+    }
+
+    //untuk mengambil jumlah
+    public int getJumlah() {
+        return jumlah;
+    }
+
+    //untuk setting jumlah
+    public void setJumlah(int jumlah) {
+        this.jumlah = jumlah;
+    }
+    
+
 }
